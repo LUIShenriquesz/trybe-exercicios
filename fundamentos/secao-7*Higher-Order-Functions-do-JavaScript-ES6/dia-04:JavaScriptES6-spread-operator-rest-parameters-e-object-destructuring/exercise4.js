fba2b508ca5fcb4c9bdc49f322a1b738deb07501
Luis Henrique <luishenrique4682@gmail.com>
2023-07-13 17:54:38 -0300

    exercicio 4

---
+++ fundamentos/secao-7*Higher-Order-Functions-do-JavaScript-ES6/dia-04:JavaScriptES6-spread-operator-rest-parameters-e-object-destructuring/exercise4.js
@@ -0,0 +1,34 @@
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+    // bornIn: nascido em
+  ];
+  
+// escreva filterPeople abaixo
+const filterPeople = (people) => people.filter(({ nationality }) => nationality === 'Australian')
+    .filter(({ bornIn }) => bornIn >= 1901 && bornIn <= 2002);
+
+console.log(filterPeople(people));
